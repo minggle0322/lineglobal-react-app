84b46499f4718cdf6f81d01be121c1371148c4cc
cyberchloe02 <chloe@lineglobal17.com>
2022-08-12 15:43:44 +0900

    main gallery img update

--- src/Main/Cards.js
+++ src/Main/Cards.js
@@ -10,6 +10,26 @@ import Photo4 from '../assets/goods/0801/tr6.jpg'
 import Photo5 from '../assets/goods/0810/25.jpg'
 import Photo6 from '../assets/goods/0810/26.jpg'
 
+
+import Photo7 from '../assets/goods/0729/tr1.png'
+import Photo8 from '../assets/goods/0801/tr2.png'
+import Photo9 from '../assets/goods/0803/5.jpg'
+import Photo10 from '../assets/goods/0803/6.jpg'
+import Photo11 from '../assets/goods/0803/7.jpg'
+import Photo12 from '../assets/goods/0803/8.jpg'
+import Photo13 from '../assets/goods/0810/27.jpg'
+import Photo14 from '../assets/goods/0810/28.jpg'
+import Photo15 from '../assets/goods/0810/29.jpg'
+import Photo16 from '../assets/goods/0810/30.jpg'
+import Photo17 from '../assets/goods/0810/31.jpg'
+import Photo18 from '../assets/goods/0810/32.jpg'
+import Photo19 from '../assets/goods/0810/33.jpg'
+import Photo20 from '../assets/goods/0810/34.jpg'
+import Photo21 from '../assets/goods/0810/35.jpg'
+import Photo22 from '../assets/goods/0810/36.jpg'
+import Photo23 from '../assets/goods/0810/37.jpg'
+import Photo24 from '../assets/goods/0803/9.jpg'
+
 function Card(props) {
     return <div className='Card'>
         <div className="thumb-cont">
@@ -39,6 +59,15 @@ function CardBundle(props) {
                     <Card src={Photo4} title="White Shortsleeve" content="" />
                     <Card src={Photo5} title="Women Dress" content="" />
                     <Card src={Photo6} title="Golf glove" content="" />
+                    <Card src={Photo7} title="" content="" />
+                    <Card src={Photo8} title="" content="" />
+                    <Card src={Photo9} title="" content="" />
+                    <Card src={Photo10} title="" content="" />
+                    <Card src={Photo11} title="" content="" />
+                    <Card src={Photo12} title="" content="" />
+                    <Card src={Photo13} title="" content="" />
+                    <Card src={Photo14} title="" content="" />
+                    <Card src={Photo15} title="" content="" />
                     <Card />
 
 
@@ -138,7 +167,6 @@ function CardBundle(props) {
 }
 
 function Cards() {
-
 
     let [Tab, ChangeTab] = useState(0);
     return (
