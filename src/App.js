f22c5bbe9b4775b6557fee0ce0b15812850d63f6
minggle0322 <jaydenpark@lineglobal17.com>
2022-09-16 18:31:04 +0900

    Revert "Language section added"
    This reverts commit 8a68db9ef8c7fd87d74189bd90dd9380a1aba595.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Notice from './Notice/index.js';
 
 import Visit from './Visit/index.js';
 import NotFound from './NotFound';
-import Language from './Language/index.js';
 
 import Main_en from './translation/Main/index.js';
 import Half_en from './translation/Half/index.js';
@@ -27,7 +26,6 @@ function App() {
                     <Route path="/notice" element={<Notice />} />
 
                     <Route path="/visit" element={<Visit />} />
-                    <Route path="/language" element={<Language />} />
                     <Route path="/en" element={<Main_en/>} />
                     <Route path="/en/halfandhalf" element={<Half_en/>} />
                     <Route path="/en/visit" element={<Visit_en/>} />
