7f0c9b11da94375a6660d14803bcb2c2b5e8a790
minggle0322 <jaydenpark@lineglobal17.com>
2023-02-08 13:24:24 +0900

    mobile patch

--- src/Notice/index.js
+++ src/Notice/index.js
@@ -150,6 +150,95 @@ function Notice() {
 
                 </div>
             </section>
+            <section className="p-mob Slide">
+            <div className="p-cont-mob">
+                        <div className="p-cont-mob-title">
+                            <span>제품 생산 과정</span>
+                        </div>
+                        <div className="p-cont-mob-sub">
+                            <span>1:1 상담을 통해 클라이언트와 소비자 모두 만족하는 결과물을 만듭니다.
+                                제품 품목 또는 디자인에 따라 제작과정은 축소될 수 있습니다.
+                            </span>
+                        </div>
+                    </div>
+            <div className="p-sub2-tables">
+                    <div className="p-sub2-table">
+                        <div className="p-sub2-title">
+                            <span>기획 및 개발</span>
+                        </div>
+                        <div className="p-sub2-sub">
+                            <span>클라이언트의 니즈에 따라 제품(디자인)과 제작 틀을 잡습니다.</span>
+                        </div>
+                    </div>
+                    <div className="arrow-angle"></div>
+                    <div className="p-sub2-table">
+                        <div className="p-sub2-title">
+                            <span>원단, 부자재 서치</span>
+                        </div>
+                        <div className="p-sub2-sub">
+                            <span>제품의 디자인, 질 및 가격을 고려하여 맞는 원단을 찾아 선택합니다.</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="p-sub2-tables">
+                    <div className="p-sub2-table">
+                        <div className="p-sub2-title">
+                            <span>제작 준비</span>
+                        </div>
+                        <div className="p-sub2-sub">
+                            <span>패턴, 샘플 제작 및 도안작업을 통해 작업지시서를 만듭니다.</span>
+                        </div>
+                    </div>
+                    <div className="arrow-angle"></div>
+                    <div className="p-sub2-table">
+                        <div className="p-sub2-title">
+                            <span>제작 의뢰</span>
+                        </div>
+                        <div className="p-sub2-sub">
+                            <span>공장에 제품 제작의뢰를 맡깁니다.</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="p-sub2-tables2">
+                    <div className="p-sub2-table">
+                        <div className="p-sub2-title">
+                            <span>제작</span>
+                        </div>
+                        <div className="p-sub2-sub">
+                            <span>기한 내에 제품을 제작합니다.</span>
+                        </div>
+                    </div>
+                    <div className="arrow-angle"></div>
+                    <div className="p-sub2-table">
+                        <div className="p-sub2-title">
+                            <span>검수</span>
+                        </div>
+                        <div className="p-sub2-sub">
+                            <span>품질 검사를 통해 제품에 문제가 없는지 확인합니다.</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="p-sub2-tables2">
+                    <div className="p-sub2-table">
+                        <div className="p-sub2-title">
+                            <span>포장</span>
+                        </div>
+                        <div className="p-sub2-sub">
+                            <span>제품에 폴리백과 택을 부착하고 포장하여 완성합니다.</span>
+                        </div>
+                    </div>
+                    <div className="arrow-angle"></div>
+                    <div className="p-sub2-table">
+                        <div className="p-sub2-title">
+                            <span>배송</span>
+                        </div>
+                        <div className="p-sub2-sub">
+                            <span>제품을 클라이언트에게 전달합니다.</span>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
             <section className="p-sub3 Slide">
                 <div className="p-sub3-cont">
                     <div className="p-sub3-text">
